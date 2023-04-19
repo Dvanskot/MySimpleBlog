3023e8c24ccecdaa743e2f5ea4e9ce554eb0f3dd
Lancelot Tshikota <dvanskot@gmail.com>
2023-04-19 10:18:37 +0200

    Nuget Packages installed

---
+++ MySimpleBlog/MySimpleBlog/Shared/Models/BlogContact.cs
@@ -0,0 +1,19 @@
+﻿using MySimpleBlog.Shared.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySimpleBlog.Shared.Models
+{
+    public class BlogContact : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string ContactNumber { get; set; }
+        public string Message { get; set; }
+        public virtual DateTimeOffset DateCreated { get; set; }
+    }
+}
---
+++ MySimpleBlog/MySimpleBlog/Shared/Models/NewsArticle.cs
@@ -0,0 +1,21 @@
+﻿using MySimpleBlog.Shared.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySimpleBlog.Shared.Models
+{
+    public class NewsArticle : BaseEntity
+    {
+        
+        public virtual string Title { get; set; }
+        public virtual string Author { get; set; }
+        public virtual string Content { get; set; }
+        public virtual string ImageName { get; set; }
+        public virtual DateTimeOffset DateCreated { get; set; }
+
+    }
+}
